87c7308f7925795ddc29a4e85c3771819a3a78cc
Sujay Bhakri <sujaybhakri21@gmail.com>
2024-03-20 21:45:37 +0530

    fixes

--- rps.js
+++ rps.js
@@ -10,7 +10,7 @@ optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
     image.classList.add("active");
 
-    userResult.src = cpuResult.src = "/GAME WEBSITE/rock.png";
+    userResult.src = cpuResult.src = "rock.png";
     result.textContent = "Wait...";
 
     // Loop through each option image again
@@ -34,11 +34,7 @@ optionImages.forEach((image, index) => {
       // Generate a random number between 0 and 2
       let randomNumber = Math.floor(Math.random() * 3);
       // Create an array of CPU image options
-      let cpuImages = [
-        "/GAME WEBSITE/rock.png",
-        "/GAME WEBSITE/paper.png",
-        "/GAME WEBSITE/scissors.png",
-      ];
+      let cpuImages = ["rock.png", "paper.png", "scissors.png"];
       // Set the CPU image to a random option from the array
       cpuResult.src = cpuImages[randomNumber];
 
